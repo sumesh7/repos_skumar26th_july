021c85f9f9570f92e05603d3810dd383602c6a32
unknown <AmitJindal@princenagaroutlook.onmicrosoft.com>
2025-07-26 18:41:52 +0530

    added repo

---
+++ main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "my-rg" {
+  name     = "jindal_rg"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "mystg" {
+  name                     = "tillustorage"
+  resource_group_name      = azurerm_resource_group.my-rg.name
+  location                 = azurerm_resource_group.my-rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+} 
