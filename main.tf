339009f73caee6104539aee010d0ab9ead53ff9a
sumesh7 <51188867+sumesh7@users.noreply.github.com>
2025-07-26 19:36:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "my-rg" {
-  name     = "skumar26_rg"
+  name     = "ramsita_rg"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "mystg" {
-  name                     = "pillustorage"
+  name                     = "lipustorage"
   resource_group_name      = azurerm_resource_group.my-rg.name
   location                 = azurerm_resource_group.my-rg.location
   account_tier             = "Standard"
